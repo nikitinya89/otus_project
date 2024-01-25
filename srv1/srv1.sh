614a2fc3df662f48cea4e94869c3d2175011bf79
Yuriy Nikitin <nikitinya89@gmail.com>
2024-01-25 11:49:40 +0000

    scripts added

---
+++ srv1/srv1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp ~/repo/srv1/nginx.conf /etc/nginx/nginx.conf
+chmod 644 /etc/nginx/nginx.conf
+cp ~/repo/srv1/frontend.conf /etc/nginx/sites-available/frontend.conf
+chmod 644 /etc/nginx/sites-available/frontend.conf
+rm /etc/nginx/sites-enabled/*
+ln -s /etc/nginx/sites-available/ frontend.conf /etc/nginx/sites-enabled/ frontend.conf
+systemctl reload nginx
+cp ~/repo/srv1/filebeat.yml /etc/filebeat/filebeat.yml
+chmod 644 /etc/filebeat/filebeat.yml
+systemctl restart filebeat
