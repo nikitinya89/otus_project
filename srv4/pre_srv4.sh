86e1239ceb88f25bea4c5811e9e301b1449e0f8c
Yuriy Nikitin <nikitinya89@gmail.com>
2024-01-25 17:36:18 +0000

    srv4

---
+++ srv4/pre_srv4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update && apt upgrade -y
+apt install bash-completion  -y
+mkdir repo && cd repo && git init
+git config --global user.name "Yuriy Nikitin"
+git config --global user.email nikitinya89@gmail.com
+git branch -m master main
+git remote add origin git@github.com:nikitinya89/otus_project.git
+git pull origin main
+chmod +x ~/repo/*/*.sh
---
+++ srv4/srv4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt install prometheus -y
+cp ~/repo/srv4/prometheus.yml /etc/prometheus/prometheus.yml
+chmod 644 /etc/prometheus/prometheus.yml
+systemctl daemon-reload
+systemctl start prometheus
+systemctl enable prometheus
+dpkg -i /root/repo/srv4/grafana*.deb
+apt -f install -y
+cp ~/repo/srv4/grafana.db /var/lib/grafana/
+chmod 640 /var/lib/grafana/grafana.db
+chown grafana:grafana /var/lib/grafana/grafana.db
+systemctl daemon-reload
+systemctl start grafana-server
+
