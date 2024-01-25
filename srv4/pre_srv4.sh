5ea4c9af5ed72774bafa6ba993e1bb1ed1c3fd7f
Yuriy Nikitin <nikitinya89@gmail.com>
2024-01-25 17:52:37 +0000

    scripts were edited

--- srv4/pre_srv4.sh
+++ srv4/pre_srv4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update && apt upgrade -y
-apt install bash-completion  -y
-mkdir repo && cd repo && git init
+apt install bash-completion git -y
+mkdir ~/repo && cd ~/repo && git init
 git config --global user.name "Yuriy Nikitin"
 git config --global user.email nikitinya89@gmail.com
 git branch -m master main
