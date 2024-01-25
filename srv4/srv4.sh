f4234c85351f62bd0c75e2e12d2afad2d02b0550
Yuriy Nikitin <nikitinya89@gmail.com>
2024-01-25 17:58:15 +0000

    scripts were edited

--- srv4/srv4.sh
+++ srv4/srv4.sh
@@ -4,7 +4,7 @@ apt install prometheus -y
 cp ~/repo/srv4/prometheus.yml /etc/prometheus/prometheus.yml
 chmod 644 /etc/prometheus/prometheus.yml
 systemctl daemon-reload
-systemctl start prometheus
+systemctl restart prometheus
 systemctl enable prometheus
 dpkg -i /root/repo/srv4/grafana*.deb
 apt -f install -y
