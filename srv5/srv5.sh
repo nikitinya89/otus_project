dfc3997b8240304e8ab40ac294e1dee71bc920c0
Yuriy Nikitin <nikitinya89@gmail.com>
2024-01-26 13:04:56 +0000

    acripts were edited

--- srv5/srv5.sh
+++ srv5/srv5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+apt install default-jdk -y
 dpkg -i /root/elasticsearch*.deb
 dpkg -i /root/kibana*.deb
 dpkg -i /root/logstash*.deb
-apt install default-jdk -y
 apt -f install -y
 cp ~/repo/srv5/jvm.options /etc/elasticsearch/jvm.options.d/jvm.options
 cp ~/repo/srv5/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
@@ -11,8 +11,8 @@ cp ~/repo/srv5/kibana.yml /etc/kibana/kibana.yml
 cp ~/repo/srv5/logstash.yml /etc/logstash/logstash.yml
 cp ~/repo/srv5/logstash-nginx-es.conf /etc/logstash/conf.d/logstash-nginx-es.conf
 chmod 644 /etc/elasticsearch/jvm.options.d/jvm.options
-chmod 644 /etc/elasticsearch/elasticsearch.yml
-chmod 644 /etc/kibana/kibana.yml
+chmod 660 /etc/elasticsearch/elasticsearch.yml
+chmod 660 /etc/kibana/kibana.yml
 chmod 644 /etc/logstash/logstash.yml
 chmod 644 /etc/logstash/conf.d/logstash-nginx-es.conf
 systemctl daemon-reload
