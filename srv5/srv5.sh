d339e70b0c8d216a805028f920f80c0921bea4ab
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-27 12:20:28 +0300

    Update srv5.sh

--- srv5/srv5.sh
+++ srv5/srv5.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# apt install default-jdk -y
-# dpkg -i /root/elasticsearch*.deb
-# dpkg -i /root/kibana*.deb
-# dpkg -i /root/logstash*.deb
-# apt -f install -y
 cp ~/otus_project/srv5/jvm.options /etc/elasticsearch/jvm.options.d/jvm.options
 cp ~/otus_project/srv5/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 cp ~/otus_project/srv5/kibana.yml /etc/kibana/kibana.yml
@@ -16,9 +10,8 @@ chmod 660 /etc/kibana/kibana.yml
 chmod 644 /etc/logstash/logstash.yml
 chmod 644 /etc/logstash/conf.d/logstash-nginx-es.conf
 systemctl daemon-reload
-# systemctl enable --now elasticsearch.service
-# systemctl enable --now kibana.service
-# systemctl enable --now logstash.service
 systemctl restart elasticsearch.service
 systemctl restart kibana.service
 systemctl restart logstash.service
+cd ~/otus_project/srv5/
+curl -X POST "localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@export.ndjson
