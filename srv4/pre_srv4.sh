1d626e0f905c7dbeb75400b5d2821998c7671dd8
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-27 12:55:12 +0300

    Update pre_srv4.sh

--- srv4/pre_srv4.sh
+++ srv4/pre_srv4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt update && apt upgrade -y
-apt install bash-completion git -y
-mkdir ~/repo && cd ~/repo && git init
-git config --global user.name "Yuriy Nikitin"
-git config --global user.email nikitinya89@gmail.com
-git branch -m master main
-git remote add origin git@github.com:nikitinya89/otus_project.git
-git pull origin main
-chmod +x ~/repo/*/*.sh
+apt install bash-completion git prometheus -y
+ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa <<< y
+git clone https://github.com/nikitinya89/otus_project.git
+wgen -c https://mirror.yandex.ru/mirrors/packages.grafana.com/enterprise/deb/pool/main/g/grafana/grafana_10.2.0_amd64.deb
+dpkg -i /root/grafana*.deb
+apt -f install -y
+systemctl enable --now prometheus
+systemctl enable --now grafana
