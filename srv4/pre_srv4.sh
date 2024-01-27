7ef330234a9084ebb662124f50dae429399a0022
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-27 13:40:21 +0300

    Update pre_srv4.sh

--- srv4/pre_srv4.sh
+++ srv4/pre_srv4.sh
@@ -2,6 +2,8 @@
 apt update && apt upgrade -y
 apt install bash-completion git prometheus prometheus-node-exporter -y
 ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa <<< y
+echo -e "if [ -f /etc/bash_completion ] && ! shopt -oq posix; then\n    . /etc/bash_completion\nfi" >> /root/.bashrc
+source /root/.bashrc
 git clone https://github.com/nikitinya89/otus_project.git
 chmod +x /root/otus_project/*/*.sh
 wget -c https://mirror.yandex.ru/mirrors/packages.grafana.com/enterprise/deb/pool/main/g/grafana/grafana_10.2.0_amd64.deb
