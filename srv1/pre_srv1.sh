bc8281ada36c98ed91efba760e9c87ac6be8c680
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-27 13:22:08 +0300

    Update pre_srv1.sh

--- srv1/pre_srv1.sh
+++ srv1/pre_srv1.sh
@@ -4,3 +4,7 @@ apt install bash-completion git nginx prometheus-node-exporter -y
 ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa <<< y
 git clone https://github.com/nikitinya89/otus_project.git
 chmod +x /root/otus_project/*/*.sh
+wget -c https://mirror.yandex.ru/mirrors/elastic/8/pool/main/f/filebeat/filebeat-8.9.1-amd64.deb
+dpkg -i /root/filebeat*.deb
+systemctl daemon-reload
+systemctl enable --now filebeat
