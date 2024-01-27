0e8b0c701a198379b53b8c57901f08f7f1cdb3de
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-27 17:46:55 +0300

    Update pre_srv2.sh

--- srv2/pre_srv2.sh
+++ srv2/pre_srv2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 apt update && apt upgrade -y
+apt install apache2 mysql-client mysql-server php8.1 php8.1-mysql libapache2-mod-php8.1 php8.1-cli php8.1-cgi php8.1-gd unison prometheus-node-exporter -y
+ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa <<< y
+git clone https://github.com/nikitinya89/otus_project.git
+chmod +x /root/otus_project/*/*.sh
 
 echo -e "if [ -f /etc/bash_completion ] && ! shopt -oq posix; then\n    . /etc/bash_completion\nfi" >> /root/.bashrc
 exec bash --rcfile /root/.bashrc
