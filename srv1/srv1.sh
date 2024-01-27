47b3d9d98c2b311eecfb3ed6b8758f94db527178
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-27 13:22:59 +0300

    Update srv1.sh

--- srv1/srv1.sh
+++ srv1/srv1.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-dpkg -i ~/repo/srv1/filebeat*.deb
-apt install prometheus-node-exporter -y
 cp ~/repo/srv1/nginx.conf /etc/nginx/nginx.conf
 chmod 644 /etc/nginx/nginx.conf
 cp ~/repo/srv1/frontend.conf /etc/nginx/sites-available/frontend.conf
@@ -11,8 +8,6 @@ ln -s /etc/nginx/sites-available/frontend.conf /etc/nginx/sites-enabled/frontend
 systemctl reload nginx
 cp ~/repo/srv1/filebeat.yml /etc/filebeat/filebeat.yml
 chmod 644 /etc/filebeat/filebeat.yml
+systemctl daemon-reload
 systemctl restart filebeat
 sudo filebeat modules enable nginx
-systemctl daemon-reload
-systemctl start node_exporter
-systemctl enable node_exporter
