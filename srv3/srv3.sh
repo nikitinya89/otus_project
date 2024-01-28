a71cc5bf2c8cba8805de32e506ed08aca01295ce
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-28 23:20:00 +0300

    Update srv3.sh

--- srv3/srv3.sh
+++ srv3/srv3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 rm /var/www/html/index.html
-cp /root/otus_project/srv3/mysqld.cnf /etc/mysql/mysql.conf.d
-chmod 644 /etc/mysql/mysql.conf.d
+cp /root/otus_project/srv3/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl restart mysql
 
 backup_dir="/root/otus_project/mysql_backup/"
@@ -23,9 +22,9 @@ GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
 FLUSH PRIVILEGES;"
 mysql -e "$MYSQL_COMMANDS"
 
-$DB_REPL_USER="repl"
-$DB_REPL_PASSWORD="52v#Q&Xvvs"
+DB_REPL_USER="repl"
+DB_REPL_PASSWORD="52v#Q&Xvvs"
 MYSQL_COMMANDS="STOP REPLICA;
-CHANGE REPLICATION SOURCE TO SOURCE_HOST='10.0.0.11', SOURCE_USER=$DB_REPL_USER, SOURCE_PASSWORD=$DB_REPL_PASSWORD, SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;
+CHANGE REPLICATION SOURCE TO SOURCE_HOST='10.0.0.11', SOURCE_USER='$DB_REPL_USER', SOURCE_PASSWORD='$DB_REPL_PASSWORD', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;
 START REPLICA;"
 mysql -e "$MYSQL_COMMANDS"
