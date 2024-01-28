0598607a6c0aa37ae3ab268a5029569194322f34
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-28 19:56:24 +0300

    Update srv2.sh

--- srv2/srv2.sh
+++ srv2/srv2.sh
@@ -31,3 +31,11 @@ CREATE USER '$DB_REPL_USER'@'%' IDENTIFIED BY '$DB_REPL_PASSWORD';
 GRANT ALL PRIVILEGES ON *.* TO '$DB_REPL_USER'@'%';
 FLUSH PRIVILEGES;"
 mysql -e "$MYSQL_COMMANDS"
+
+unison /var/www ssh://10.0.0.12//var/www -owner -group
+new_crontab="* * * * * unison -batch /var/www ssh://10.0.0.12//var/www &> /dev/null"
+current_crontab=$(crontab -l 2>/dev/null)
+echo "$current_crontab" > temp_crontab
+echo "$new_crontab" >> temp_crontab
+crontab temp_crontab
+rm temp_crontab
