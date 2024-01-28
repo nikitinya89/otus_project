4a476e87525b938d451c533091f72684a5acf119
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-28 23:08:34 +0300

    Delete srv2/repl_srv2-3.sh

--- srv2/repl_srv2-3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-unison /var/www ssh://10.0.0.12//var/www -owner -group
-new_crontab="* * * * * unison -batch /var/www ssh://10.0.0.12//var/www &> /dev/null"
-current_crontab=$(crontab -l 2>/dev/null)
-echo "$current_crontab" > temp_crontab
-echo "$new_crontab" >> temp_crontab
-crontab temp_crontab
-rm temp_crontab
