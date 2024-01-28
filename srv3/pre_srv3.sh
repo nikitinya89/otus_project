2f1f2f2f4078f353e82c1073eedd1bf13813d62a
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-28 23:08:54 +0300

    Update pre_srv3.sh

--- srv3/pre_srv3.sh
+++ srv3/pre_srv3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update && apt upgrade -y
-apt install bash-completion git apache2 mysql-client mysql-server php8.1 php8.1-mysql libapache2-mod-php8.1 php8.1-cli php8.1-cgi php8.1-gd unison prometheus-node-exporter -y
+apt install bash-completion git apache2 mysql-server-8.0 php8.1 php8.1-mysql libapache2-mod-php8.1 php8.1-cli php8.1-cgi php8.1-gd unison prometheus-node-exporter -y
 ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa <<< y
 git clone https://github.com/nikitinya89/otus_project.git
 chmod +x /root/otus_project/*/*.sh
