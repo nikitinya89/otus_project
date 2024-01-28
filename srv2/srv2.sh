dca0a109c34ff9fd53f0590dbf5638eb3aacde4f
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-28 17:44:50 +0300

    Update srv2.sh

--- srv2/srv2.sh
+++ srv2/srv2.sh
@@ -4,3 +4,6 @@ chown -R www-data:www-data /var/www/html/
 chmod -R 755 /var/www/html/
 rm /var/www/html/index.html
 systemct  reload apache2
+cp /root/otus_project/srv2/mysqld.cnf /etc/mysql/mysql.conf.d
+chmod 644 /etc/mysql/mysql.conf.d
+systemct  restart mysql
