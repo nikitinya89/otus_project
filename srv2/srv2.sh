d6741717389be8886e86fc2de4000f03529c9ba1
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-28 18:53:34 +0300

    Update srv2.sh

--- srv2/srv2.sh
+++ srv2/srv2.sh
@@ -6,4 +6,24 @@ rm /var/www/html/index.html
 systemct  reload apache2
 cp /root/otus_project/srv2/mysqld.cnf /etc/mysql/mysql.conf.d
 chmod 644 /etc/mysql/mysql.conf.d
-systemct  restart mysql
+systemctl restart mysql
+
+backup_dir="/root/otus_project/mysql_backup/"
+for db_dir in $(ls $backup_dir); do
+    db=$(basename $db_dir)
+    for tbl_dir in $(ls $backup_dir/$db); do
+        tbl=$(basename $tbl_dir)
+        tbl_backup="$backup_dir/$db/$tbl/$tbl.sql"
+        mysql -u root -e "CREATE DATABASE IF NOT EXISTS $db;"
+        mysql -u root $db < $tbl_backup
+    done
+done
+
+DB_NAME="wordpress_db"
+DB_USER="wordpress"
+DB_PASSWORD="2Z%Zv3W93%"
+MYSQL_COMMANDS="CREATE DATABASE $DB_NAME;
+CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
+FLUSH PRIVILEGES;"
+mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "$MYSQL_COMMANDS"
