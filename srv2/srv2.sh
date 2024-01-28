e6d78c5cfc39ea5a431986643717dfbf305e60c9
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-01-28 16:53:48 +0300

    Update srv2.sh

--- srv2/srv2.sh
+++ srv2/srv2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rsync -av /root/otus_project/srv2/wordpress/ /var/www/
-sudo chown -R www-data:www-data /var/www/html/
-sudo chmod -R 755 /var/www/html/
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
