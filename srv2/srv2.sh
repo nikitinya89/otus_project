e33fca1e1d55fa49e16bec7481b325a16c36a912
Yuriy Nikitin <nikitinya89@gmail.com>
2024-01-29 12:09:48 +0000

    srv2

--- srv2/srv2.sh
+++ srv2/srv2.sh
@@ -2,7 +2,7 @@
 rsync -av /root/otus_project/srv2/wordpress/ /var/www/
 chown -R www-data:www-data /var/www/html/
 chmod -R 755 /var/www/html/
-rm /var/www/html/index.html
+cp /root/otus_project/srv2/000-default.conf /etc/apache2/sites-available/000-default.conf
 systemctl reload apache2
 cp /root/otus_project/srv2/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl restart mysql
